298edd2f8f49c0d57bba37e96b7a1dfc52d3e072
Mykyta Zub <Mykyta_Zub@epam.com>
2025-01-29 10:03:02 +0200

    fix

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, include
-
+from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import SignUpView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
-    path("", include("django.contrib.auth.urls")),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
